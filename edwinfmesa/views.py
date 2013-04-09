41512bb2ec3a0d2c26a10bd5ec19ca84bd720e7a
Edwin Mesa <edwinfmesa@hotmail.com>
2013-04-09 16:03:11 -0500

    Exception added to problem with the connection with de database

--- edwinfmesa/views.py
+++ edwinfmesa/views.py
@@ -7,18 +7,21 @@ from pymongo import MongoClient
 def home(request):
 	#connect to database 
     # connection = MongoClient('widmore.mongohq.com',10010)
-	connection = MongoClient('localhost',27017)
-
+	try:
+		connection = MongoClient('localhost',27017)
 	
-	#	if connection.my_blog.authenticate('edwinfmesa','123456'):
-	#		db = connection.my_blog
-	#	else:
-	#		print "Error de autenticacion"
+		
+		#	if connection.my_blog.authenticate('edwinfmesa','123456'):
+		#		db = connection.my_blog
+		#	else:
+		#		print "Error de autenticacion"
+	
+		db = connection.users
+		user = db.user
 
-	db = connection.users
-	user = db.user
-
-	things = user.find()
+		things = user.find()
+	except:
+		things = [{'name':'Ha ocurrido un error con la base de datos'}]
 	# print things
 	# for thing in things:
 	# 	print thing['_id']
