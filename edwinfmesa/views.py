bb8d45c1fe75bfbd50f3ff2f4cdaaad9f3d587c7
Edwin Mesa <edwinfmesa@hotmail.com>
2013-04-10 17:09:25 -0500

    Using map reduce

--- edwinfmesa/views.py
+++ edwinfmesa/views.py
@@ -36,7 +36,7 @@ def home(request):
 				d = datetime.datetime.now()
 				print d
 				try: 
-					daily_visits.insert({'date': d})
+					daily_visits.insert({'my_id':"abc%s"%(int(random.random()*10)),'date': d})
 					# for i in range(5):
 					# 	x = "2013-04-%s"%(int(random.random()*24)+1)
 					# 	f = datetime.datetime.strptime(x,"%Y-%m-%d")
@@ -62,72 +62,34 @@ def home(request):
 def visits(request):
 	try:
 		connection = MongoClient('localhost',27017)
-	
-		
-		#	if connection.my_blog.authenticate('edwinfmesa','123456'):
-		#		db = connection.my_blog
-		#	else:
-		#		print "Error de autenticacion"
-	
 		db = connection.users
-		# user = db.user
-		# if request.method == "GET":  # envia una variable para seleccionar una organizacion
-		# 	try:
-		# 		name = request.GET['new_user']
-		# 		try:
-		# 			user.insert({'name':name})
-		# 		except:
-		# 			print 'Error intentando guardar dato'
-		# 	except Exception:
-		# 		print 'No hay variable get: new_user'
-		daily_visits = db.daily_visits
-		# daily_visits.insert({'date': datetime.date.now()})
-		try:
-			visits2 = daily_visits.find().sort([("date",-1)])
-			visits = daily_visits.find().sort([("date",1)])
-			print '---------------------------------------',visits
-			for i in visits2:
-				print i
-		except:
-			print 'Error', sys.exc_info()[0]
 	except:
-		visits = [{}]
 		print 'error', sys.exc_info()[0]
 
-	visitas = {}
-	for v in visits:
-		try:
-			visitas[datetime.datetime.strftime(v['date'],"%d")]=visitas[datetime.datetime.strftime(v['date'],"%d")]+1
-		except:
-			visitas[datetime.datetime.strftime(v['date'],"%d")]=1
-	print '------------------------'
-	for p in visitas:
-		print p
-	datos = []
-	for v in visitas:
-		datos.append([v,visitas[v]])
-	print '------------------------'
-	for p in datos:
-		print p
-
-	# from bson.code import Code
-	# map = Code("function () {"
- #            "  this.date.forEach(function(z) {"
- #            "    emit(z, 1);"
- #            "  });"
- #           "}")
-	# reduce = Code("function (key, values) {"
- #               "  var total = 0;"
- #               "  for (var i = 0; i < values.length; i++) {"
- #               "    total += values[i];"
- #               "  }"
- #               "  return total;"
- #              "}")
-	# result = db.daily_visits.map_reduce(map, reduce, "myresults")
-	# print visitas
-	# print things
-	# for thing in things:
-	# 	print thing['_id']
-	ctx = {'visitas':datos}
-
+	# Map Reduce Operations
+	from bson.code import Code
+	map = Code("function () {"
+			"var key = this.my_id;"
+			"var values = {'id': this.my_id, count: 1, date: [this.date] };"
+	        "    emit(key,values);"
+	       "}")
+	reduce = Code("function (key, values) {"
+	           "  var total = 0;"
+	           "  var reducedValue = {'id':key, 'count':0, 'date':[]};"
+	           "  for (var i = 0; i < values.length; i++) {"
+	           "    reducedValue['count'] += parseInt(values[i].count);"
+	           "    reducedValue['date'].push(values[i].date[0]);"
+	           "  }"
+	           "  return reducedValue;"
+	          "}")
+	result = db.daily_visits.map_reduce(map, reduce, "myresults")
+	firstmr = result.find()
+	data = []
+	print " \n ------------MapReduce------------------ \n"
+	for i in firstmr:
+		data.append(i)
+		print "\n------------------------------------------------\n",i	
+	# end Map Reduce
+	
+	ctx = {'datos':data}
 	return render_to_response('edwinfmesa/visits.html', ctx, context_instance=RequestContext(request))
